6258213466e0c6f0da633d722780fcbd8a1f68e5
Colin Alworth <colin@colinalworth.com>
2018-06-13 09:12:12 -0500

    Quick hack to make it work in j2cl

--- src/main/java/com/axellience/vuegwtdemo/client/VueGwtDemoApp.java
+++ src/main/java/com/axellience/vuegwtdemo/client/VueGwtDemoApp.java
@@ -4,12 +4,12 @@ import com.axellience.vuegwt.core.client.Vue;
 import com.axellience.vuegwt.core.client.VueGWT;
 import com.axellience.vuegwtdemo.client.components.counter.CounterComponent;
 import com.axellience.vuegwtdemo.client.components.todolist.TodoListComponent;
-import com.google.gwt.core.client.EntryPoint;
+//import com.google.gwt.core.client.EntryPoint;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>
  */
-public class VueGwtDemoApp implements EntryPoint
+public class VueGwtDemoApp/* implements EntryPoint*/
 {
     /**
      * This is the entry point method.
